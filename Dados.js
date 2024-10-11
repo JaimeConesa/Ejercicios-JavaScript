8e4ce8e61fa2d1c69ec18b8e66cd56ae3d78a2bf
Jaime Conesa Ponce <168517014+JaimeConesa@users.noreply.github.com>
2024-10-11 23:17:23 +0200

    Dados.js

--- Dados.js
+++ Dados.js
@@ -1,3 +1,8 @@
+/*
+Crea una función que simule el lanzamiento de dos dados (números
+aleatorios entre 1 y 6). El programa debe contar cuántas veces se obtiene un total de
+7 al lanzar los dados 100 veces.
+*/
 let siete = 0;
 function lanzaDados(){
     dado1 = Math.floor(Math.random() * 6) + 1;
